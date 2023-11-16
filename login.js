c17a8740d9e1dde4e685a632d33a49d11eea0820
Kally-Dossa <ics21148@uom.edu.gr>
2023-11-16 21:39:48 +0200

    welcome and login pages

--- login.js
+++ login.js
@@ -1,6 +1,26 @@
 'use strict'
 
 function submitBtn() {
-    const username = document.getElementById("username");
-    window.location.href = 'teacher.html';
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            if (checkbox.id === 'student') {
+                window.location.href = 'student.html';
+            }
+            else if (checkbox.id === 'teacher') {
+                window.location.href = 'teacher.html';
+            }
+        }
+    });
+}
+
+function handleCheckboxClick(clickedCheckboxId) {
+    const checkboxes = document.querySelectorAll('.form-check-input');
+
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.id !== clickedCheckboxId) {
+            checkbox.checked = false;
+        }
+    });
 }
