1951298171afa431e9f075ee471999de182652ee
Kally-Dossa <ics21148@uom.edu.gr>
2023-11-19 22:19:37 +0200

    student update

---
+++ student.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function togglePanel(panelId) {
+    var panel = document.getElementById(panelId);
+    if (panel.style.display === "none") {
+        panel.style.display = "block";
+    } else {
+        panel.style.display = "none";
+    }
+}
