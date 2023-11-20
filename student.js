91fca63d41930120cf88df36f1ff64189ac6a34f
Kally-Dossa <ics21148@uom.edu.gr>
2023-11-20 18:56:47 +0200

    proxeiro update

--- student.js
+++ student.js
@@ -1,10 +1,1 @@
 'use strict'
-
-function togglePanel(panelId) {
-    var panel = document.getElementById(panelId);
-    if (panel.style.display === "none") {
-        panel.style.display = "block";
-    } else {
-        panel.style.display = "none";
-    }
-}
