e327e3311ec783d9f1dffb844b54c3a5a26b2664
Lefki Violari <lviolari2@gmail.com>
2023-11-22 13:02:58 +0200

    update teacher

--- teacher.js
+++ teacher.js
@@ -1,5 +1,26 @@
 'use strict'
 
+let currentSlide = 1;
+
 function showMessage(action) {
     alert('Toolbar button clicked: ' + action);
 }
+function showSlide(slideNumber) {
+    const slides = document.querySelectorAll('.slide');
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+
+    const currentSlideElement = document.getElementById(`slide${slideNumber}`);
+    if (currentSlideElement) {
+        currentSlideElement.style.display = 'block';
+        currentSlide = slideNumber;
+    }
+}
+
+function changeSlide(nextSlide) {
+    showSlide(nextSlide);
+}
+
+// Show the initial slide
+showSlide(currentSlide);
