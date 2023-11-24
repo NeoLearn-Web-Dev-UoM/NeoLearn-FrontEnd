a7d80f557e2272689d7d015e35048e009f2c27ef
Kally-Dossa <ics21148@uom.edu.gr>
2023-11-25 00:38:50 +0200

    student and lesson details update

--- student.js
+++ student.js
@@ -1,6 +1,5 @@
 'use strict'
 let currentSlide = 1;
-
 function showSlide(slideNumber) {
     const slides = document.querySelectorAll('.slide');
     slides.forEach(slide => {
@@ -13,11 +12,18 @@ function showSlide(slideNumber) {
         currentSlide = slideNumber;
     }
 }
-
 function changeSlide(nextSlide) {
     showSlide(nextSlide);
 }
-
 // Show the initial slide
 showSlide(currentSlide);
 
+
+function lessonClicked(lessonTitle) {
+
+    //για να ξερω αν πηρε σωστο ονομα μαθηματος θα το σβησω 
+    alert('Lesson Clicked: ' + lessonTitle);
+    var details = document.getElementById('lesson-name')
+    details.textContent = lessonTitle;
+
+}
