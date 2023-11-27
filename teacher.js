e23f4459b8eefb8fcc1a4c8cac54acf4e29afd6c
Lefki Violari <lviolari2@gmail.com>
2023-11-27 17:15:38 +0200

    δες το teacher, εβαλα container δεν εχω ιδέα αν δουλέυει, φαίνεται πως ναι

--- teacher.js
+++ teacher.js
@@ -1,10 +1,6 @@
 'use strict'
-
 let currentSlide = 1;
 
-function showMessage(action) {
-    alert('Toolbar button clicked: ' + action);
-}
 function showSlide(slideNumber) {
     const slides = document.querySelectorAll('.slide');
     slides.forEach(slide => {
@@ -24,3 +20,12 @@ function changeSlide(nextSlide) {
 
 // Show the initial slide
 showSlide(currentSlide);
+
+function lessonClicked(lessonTitle) {
+
+    //για να ξερω αν πηρε σωστο ονομα μαθηματος θα το σβησω 
+    alert('Lesson Clicked: ' + lessonTitle);
+    var details = document.getElementById('lesson-name')
+    details.textContent = lessonTitle;
+
+}
