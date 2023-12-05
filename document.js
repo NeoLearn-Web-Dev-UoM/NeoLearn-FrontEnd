bc6d00d60d08beed43aa1ae780c26f0495d925c9
Kally-Dossa <ics21148@uom.edu.gr>
2023-12-05 15:17:43 +0200

    create document and lecture pages

---
+++ document.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let currentSlide = 1;
+function showSlide(slideNumber) {
+    const slides = document.querySelectorAll('.slide');
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+
+    const currentSlideElement = document.getElementById(`slide${slideNumber}`);
+    if (currentSlideElement) {
+        currentSlideElement.style.display = 'block';
+        currentSlide = slideNumber;
+    }
+}
+function changeSlide(nextSlide) {
+    showSlide(nextSlide);
+}
+// Show the initial slide
+showSlide(currentSlide);
+
+
+function lecture(lecture) {
+    window.location.href = 'lecture.html';
+}
