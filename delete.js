d8401eae9f5d5cd3bb3d96187c755fdd376d96d1
Kally-Dossa <ics21148@uom.edu.gr>
2023-12-15 22:46:41 +0200

    update delete btn

--- delete.js
+++ delete.js
@@ -5,3 +5,10 @@ function deleteLessonLink(event) {
     event.preventDefault();
     window.location.href = 'deleteLesson.html';
 }
+
+function showAlert() {
+    alert("Το μάθημα διαγράφτηκε με επιτυχία!");
+    //εδω θυελω οταν παταω οκ στο αλερτ να νμε οηγαινει στην αρχικη σελιδα με τα μαθηματα χωωρις το μαθηα 
+    // που μολις διεγραψα 
+    window.location.href = 'student.html';
+}
