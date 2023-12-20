4e60c055e6c90e46f17485356ce3266af84ab1c3
Lefki Violari <lviolari2@gmail.com>
2023-12-20 15:12:54 +0200

    update

---
+++ deleteTeacher.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//οταν παταει απεγγραφη στο παραθυρο με τη περιγραφη του μαθηματος
+function deleteLessonLink(event) {
+    event.preventDefault();
+    window.location.href = 'deleteLesson.html';
+}
+
+function showAlert() {
+    alert("Το μάθημα διαγράφτηκε με επιτυχία!");
+    //εδω θυελω οταν παταω οκ στο αλερτ να νμε οηγαινει στην αρχικη σελιδα με τα μαθηματα χωωρις το μαθηα 
+    // που μολις διεγραψα 
+    window.location.href = 'teacher.html';
+}
