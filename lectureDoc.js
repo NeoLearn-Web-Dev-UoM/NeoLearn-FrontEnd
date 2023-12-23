a0acfa3267d0acc9fb0f39f84490fa35d50304ba
Lefki Violari <lviolari2@gmail.com>
2023-12-23 16:07:46 +0200

    complete lectureDoc

--- lectureDoc.js
+++ lectureDoc.js
@@ -98,3 +98,62 @@ function getFileIcon(fileType) {
         // Προσθέστε άλλες περιπτώσεις ανάλογα με τους διαφορετικούς τύπους αρχείων
     }
 }
+
+function createNewFolderRow() {
+    // Επιλέγουμε τον πίνακα
+    var table = document.getElementById("lecturesTable");
+
+    // Δημιουργούμε μια νέα γραμμή
+    var row = table.insertRow(table.rows.length);
+
+    // Προσθέτουμε τα κελιά
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+
+    // Προσθέτουμε τα περιεχόμενα των κελιών
+    cell1.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-folder2-open" viewBox="0 0 16 16"><path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"/></svg>';
+
+    // Δημιουργούμε το input field για το όνομα του φακέλου
+    var inputField = document.createElement('input');
+    inputField.type = 'text';
+    inputField.id = 'folderNameInput';
+    inputField.placeholder = 'Enter folder name';
+
+    // Δημιουργούμε το κουμπί για τη δημιουργία φακέλου
+    var createButton = document.createElement('button');
+    createButton.textContent = 'Create Folder';
+   // Εκτελεί τη συνάρτηση όταν πατηθεί το κουμπί
+createButton.onclick = function () {
+    // Εδώ μπορείτε να προσθέσετε τον κώδικα που θέλετε για τη δημιουργία του φακέλου
+    alert('Create folder: ' + inputField.value);
+
+    // Εξαφανίζουμε το πλαίσιο κειμένου και το κουμπί
+    inputField.style.display = 'none';
+    createButton.style.display = 'none';
+
+    // Δημιουργούμε ένα νέο span για το κείμενο
+    var textSpan = document.createElement('span');
+    textSpan.textContent = inputField.value;
+    textSpan.style.cursor = 'pointer';
+    textSpan.style.textDecoration = 'underline';
+    textSpan.style.color = 'blue';
+
+    // Εμφανίζουμε το span με το κείμενο στο κελί
+    cell2.innerHTML = '';
+    cell2.appendChild(textSpan);
+};
+
+    // Προσθέτουμε το input field και το κουμπί στο κελί
+    cell2.appendChild(inputField);
+    cell2.appendChild(createButton);
+
+    cell3.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>';
+
+    // Προσθέτουμε την ημερομηνία δημιουργίας του φακέλου
+    var currentDate = new Date();
+    var formattedDate = currentDate.toLocaleDateString();
+    cell4.innerHTML = formattedDate;
+}
+
