4074642e59b4375d0b0ecfea83906b2d89dd7a37
Lefki Violari <lviolari2@gmail.com>
2024-01-09 02:07:40 +0200

    delete

--- lecturesTeacher.js
+++ lecturesTeacher.js
@@ -72,10 +72,15 @@ function addTextboxq(event) {
         // Create a new cell for the date
         var dateCell = document.createElement("td");
         dateCell.textContent = date;
-
+        var svgCell = document.createElement("td");
+        svgCell.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-folder-x" viewBox="0 0 16 16" style="cursor:pointer;" onclick="deleteRow(this, event)">
+                                <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181L15.546 8H14.54l.265-2.91A1 1 0 0 0 13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91H9v1H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zm6.339-1.577A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707"/>
+                                <path d="M11.854 10.146a.5.5 0 0 0-.707.708L12.293 12l-1.146 1.146a.5.5 0 0 0 .707.708L13 12.707l1.146 1.147a.5.5 0 0 0 .708-.708L13.707 12l1.147-1.146a.5.5 0 0 0-.707-.708L13 11.293l-1.146-1.147z"/>
+                            </svg>`;
         // Append cells to the new row
         newRow.appendChild(courseCell);
         newRow.appendChild(dateCell);
+        newRow.appendChild(svgCell);
 
         newRow.addEventListener('click', function() {
             // Μεταφορά του χρήστη στη σελίδα "lectureDoc.html"
@@ -112,4 +117,12 @@ function logOutBtn() {
       window.location.href = 'welcome.html';
     }
     document.getElementById("demo").innerHTML = txt;
+}
+
+function deleteRow(element, event) {
+    var row = element.closest('tr');
+    row.remove();
+
+    // Σταματήστε το γεγονός κλικ να φτάσει στα επίπεδα που θα προκαλέσουν τη μετάβαση στην άλλη σελίδα
+    event.stopPropagation();
 }
