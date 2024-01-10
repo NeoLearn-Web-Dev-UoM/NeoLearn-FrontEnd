f901ac3afff809bc090bb706154ca665713f6615
Kally Dossa <85954491+Kally-Dossa@users.noreply.github.com>
2024-01-10 23:34:43 +0200

    update

--- quiz.js
+++ quiz.js
@@ -1,25 +1,60 @@
 'use strict'
 
-function tryQuiz(event) {
+document.getElementById("quiz-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Αποτρέπει την προεπιλεγμένη συμπεριφορά της φόρμας (αποστολή και ανανέωση της σελίδας)
+    calculateScore();
+});
+
+function calculateScore() {
     event.preventDefault();
-    window.location.href = 'quizLesson.html';
+
+    const correctAnswers = ["c", "c", "b", "b", "b", "c", "b", "b", "a", "c"];
+    let score = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        const selectedAnswer = document.querySelector(`input[name="q${i}"]:checked`);
+
+        if (selectedAnswer) {
+            const userAnswer = selectedAnswer.value;
+
+            if (userAnswer === correctAnswers[i - 1]) {
+                score++;
+            }
+        }
+    }
+
+    alert(`Σκορ: ${score}/10`);
 }
 
+let duration = 30 * 60;
 
-function openNewPage(lessonTextId, sourceTitleId) {
-    var lessonText = document.getElementById(lessonTextId).textContent;
-    var sourceTitle = document.getElementById(sourceTitleId).textContent;
+// Επιλέγουμε το στοιχείο του χρονομέτρου
+const timerElement = document.getElementById("timer");
 
-    // Use unique identifiers for each link
-    var keyPrefix = 'lessonDescription_';  // Unique prefix
-    localStorage.setItem(keyPrefix + 'lessonText', lessonText);
-    localStorage.setItem(keyPrefix + 'sourceTitle', sourceTitle);
+// Ορίζουμε μια συνάρτηση για την ενημέρωση του χρονομέτρου
+function updateTimer() {
+    const minutes = Math.floor(duration / 60);
+    const seconds = duration % 60;
 
-    // Navigate to the new_page.html
-    window.location.href = 'lessonDescription.html';
+    // Εμφανίζουμε τον χρόνο στο στοιχείο του χρονομέτρου
+    timerElement.textContent = ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+
+    // Μειώνουμε τον χρόνο κατά ένα δευτερόλεπτο
+    duration--;
+
+    // Εάν ο χρόνος έχει λήξει, μπορείτε να προσθέσετε κώδικα εδώ για επιπλέον λειτουργίες
+
+    // Εάν ο χρόνος δεν έχει λήξει, ορίζουμε την επόμενη κλήση της συνάρτησης μετά από ένα δευτερόλεπτο
+    if (duration >= 0) {
+        setTimeout(updateTimer, 1000);
+    }
 }
+
+// Καλούμε τη συνάρτηση για πρώτη φορά για να ξεκινήσει το χρονόμετρο
+updateTimer();
+
 function homeBtn() {
-    window.location.href = 'student.html';
+    window.location.href = 'teacher.html';
 }
 
 function logOutBtn() {
@@ -28,4 +63,9 @@ function logOutBtn() {
         window.location.href = 'welcome.html';
     }
     document.getElementById("demo").innerHTML = txt;
+}
+
+function quizLessonLink(event) {
+    event.preventDefault();
+    window.location.href = "quizTeacher.html";
 }
